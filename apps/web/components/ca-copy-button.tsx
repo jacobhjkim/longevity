e60281c64cdf31eb07aee9998b5f47a98580ace1
Jacob Kim <hello@jacob.kim>
2025-01-13 01:26:31 +0900

    feat: update ca button

--- apps/web/components/ca-copy-button.tsx
+++ apps/web/components/ca-copy-button.tsx
@@ -3,8 +3,8 @@
 import { Button } from '@repo/ui/button'
 import { Check, Copy } from 'lucide-react'
 import { useEffect, useState } from 'react'
-import { useCopyToClipboard } from '~/lib/utils'
 import { siteConfig } from '~/config/site'
+import { useCopyToClipboard } from '~/lib/utils'
 
 export function CACopyButton() {
   const [isCopied, setIsCopied] = useState(false)
@@ -30,6 +30,9 @@ export function CACopyButton() {
 
   return (
     <Button variant='ghost' className='font-mono' onClick={handleClick}>
+      <span>
+        {siteConfig.ca.slice(0, 4)}...{siteConfig.ca.slice(siteConfig.ca.length - 4, siteConfig.ca.length)}
+      </span>
       {isCopied ? <Check className='h-4 w-4 text-green-500' /> : <Copy className='h-4 w-4' />}
     </Button>
   )
