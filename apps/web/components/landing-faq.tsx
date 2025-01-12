61713fcc1ee663caa76af8f6c87762746c493ef4
Jacob Kim <hello@jacob.kim>
2025-01-13 03:29:58 +0900

    chore: update tokenomics faq

--- apps/web/components/landing-faq.tsx
+++ apps/web/components/landing-faq.tsx
@@ -6,16 +6,6 @@ export function LandingFaq() {
     <div className='flex w-full max-w-screen-xl flex-col gap-4 px-6 py-4 md:px-10 lg:px-12'>
       <h2 className='font-semibold text-3xl'>Frequently Asked Questions</h2>
       <Accordion type='single' collapsible className='w-full'>
-        <AccordionItem value='item-0'>
-          <AccordionTrigger>How do I claim $LONGAI token airdrop?</AccordionTrigger>
-          <AccordionContent>
-            Since this project was initially inspired by{' '}
-            <a href='https://yesnoerror.com/' target='_blank' rel='noreferrer'>
-              yesnoerror
-            </a>
-            , $YNE holders can claim $LONGAI airdrop via Helius' Airship.
-          </AccordionContent>
-        </AccordionItem>
         <AccordionItem value='item-1'>
           <AccordionTrigger>What is $LONGAI token's CA?</AccordionTrigger>
           <AccordionContent>
@@ -25,8 +15,14 @@ export function LandingFaq() {
         <AccordionItem value='item-2'>
           <AccordionTrigger>What's the token distribution plan?</AccordionTrigger>
           <AccordionContent>
-            We will reserve 8% of the total supply for the team and 2% for the AI model fee. 10% for the airdrop, and
-            the rest for fair launch on Pump.fun.
+            <ul className='list-disc pl-4'>
+              <li>
+                The team owns approximately 8% of the total supply. Of that 8%, half is locked for three months.
+              </li>
+              <li>
+                An additional 10% of the total supply will be allocated to support this project and longevity research.
+              </li>
+            </ul>
           </AccordionContent>
         </AccordionItem>
       </Accordion>
